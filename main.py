8c3f7f6a46ec2ae8f16b47abc55fa9e5c0657aa4
Burak <eselik.burak@gmail.com>
2022-03-26 14:27:31 +0300

    Added main.py, aim and info about the program!

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Aim of this program: 
+Get announcements and news from https://ogrisl.erciyes.edu.tr/DYRT/All-News/0
+And send mail to given e-mail address that the latest announcement.
+"""
+
+#date: 3 march 2022
+#author: BurakEselik
+
