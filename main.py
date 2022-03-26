b328a4b10ddd9bccfdd741dff72ec1c0b9865e3a
Burak <eselik.burak@gmail.com>
2022-03-26 19:15:45 +0300

    edited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Aim of this program: 
-Get announcements and news from https://ogrisl.erciyes.edu.tr/DYRT/All-News/0
+Get announcements and news from https://ogrisl.erciyes.edu.tr/DYRT/Tum-Duyuru-ve-Haberler/0
 And send mail to given e-mail address that the latest announcement.
 """
 
