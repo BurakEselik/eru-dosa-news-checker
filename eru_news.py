fe837b9f3d41845663762db89bd712ef39259911
Burak <eselik.burak@gmail.com>
2022-03-28 12:58:09 +0300

    added while loop inside eru_news and renamed main.py to eru_news.py

--- eru_news.py
+++ eru_news.py
@@ -10,12 +10,13 @@ import json
 import news
 import mail
 from news import BASE_URL
+from time import sleep
+
 
 def getDifferenceNumber() -> int:
     with open("old_news.json", "r", encoding="utf-8") as old_news, open("new_news.json", "r", encoding="utf-8") as new_news:
         old = json.load(old_news)
         new = json.load(new_news)
-
         for i in range(1, 11):
             if old["1"]["date"] == new[str(i)]["date"] and old["1"]["title"] == new[str(i)]["title"]:
                 return i
@@ -27,8 +28,8 @@ def setContent(df_number) -> dict:
         new = json.load(new_news)
         for i in range(1, df_number):    
             content_dict[str(i)] = new[str(i)]
-
     return content_dict
+
 
 def sendMail(content_dict) -> None:
     new_mail = mail.SendMail()
@@ -43,14 +44,13 @@ def main() -> None:
     new_news.run
     df_number = getDifferenceNumber()
     if df_number > 1:
-        #print(f"Yeni {df_number - 1} tane duyuru tespit ettik.")
         content_dict = setContent(df_number=df_number)
         sendMail(content_dict)
     else:
-        #print("Yeni bir duyuru yok!")
         pass
 
 
-
 if __name__ == "__main__":
-    main()
+    while 1:
+        main()
+        sleep(600)
